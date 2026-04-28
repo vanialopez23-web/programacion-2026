f270370edee0a5df229345672e0fed261dce1c11
vanialopez23-web <vanialopez23@ciencias.unam.mx>
2026-04-28 17:30:18 -0600

    Add files via upload

---
+++ MD2.1_OperadorPunto
@@ -0,0 +1,9 @@
+'''
+autor: vanialopez23@ciencias.unam.mx
+'''
+
+class Cuenta:
+    def __init__(self, nombre, valor, tipo):
+        self.nombre = nombre
+        self.valor = valor
+        self.tipo = tipo
---
+++ teoria/Prueba.py
@@ -0,0 +1,7 @@
+#Una vez creada la clase cuenta, asignaremos atributos e imprimiremos
+from Cuenta import *
+
+Cuenta1 = Cuenta("Carmen", 600, "H")
+print("Nombre asociado:",Cuenta1.nombre)
+print("Saldo de la cuenta:",Cuenta1.valor)
+print("Tipo de la cuenta:",Cuenta1.tipo)
