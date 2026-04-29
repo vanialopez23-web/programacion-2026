cd0de65117a15857fa384c917b872030393ab987
vanialopez23-web <vanialopez23@ciencias.unam.mx>
2026-04-28 19:09:22 -0600

    Delete teoria directory

--- teoria/Prueba.py
+++
@@ -1,7 +0,0 @@
-#Una vez creada la clase cuenta, asignaremos atributos e imprimiremos
-from Cuenta import *
-
-Cuenta1 = Cuenta("Carmen", 600, "H")
-print("Nombre asociado:",Cuenta1.nombre)
-print("Saldo de la cuenta:",Cuenta1.valor)
-print("Tipo de la cuenta:",Cuenta1.tipo)
