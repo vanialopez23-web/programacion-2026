44ab77ad051f173a56486c79ab4df3374ef30850
vanialopez23-web <vanialopez23@ciencias.unam.mx>
2026-06-07 23:35:31 -0600

    Create CuentaCredito.py

---
+++ Teoría/5.1_Herencia/CuentaCredito.py
@@ -0,0 +1,27 @@
+from Cuenta import *
+
+class CuentaCredito(Cuenta):
+    def __init__(self, tipo, valor, fecha1, mSobregiro):
+        super().__init__(tipo, valor, fecha1)
+        self.montoSobregiro = mSobregiro
+
+    def __str__(self):
+        msg = super().__str__()
+        msg += "\nMonto de Sobregiro: " + str(self.montoSobregiro)
+        return msg
+
+    def retirar(self, cantidad):
+        saldo = self.get_valor()
+        if saldo >= cantidad:
+            return super().retirar(cantidad)
+
+        sobregiro_necesario = cantidad - saldo
+
+        if self.montoSobregiro >= sobregiro_necesario:
+            super().retirar(saldo)
+            self.montoSobregiro -= sobregiro_necesario
+            return True
+
+        else:
+            print("No se pudo retirar")
+            return False
